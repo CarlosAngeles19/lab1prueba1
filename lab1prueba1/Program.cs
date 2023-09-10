9879375026cfacf1fe0f87cf38c6ab31a2371e9b
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-09 23:31:34 -0500

    Agregar archivos de proyecto.

---
+++ lab1prueba1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("digite la cantidad de minutos: ");
+        int minutos = int.Parse(Console.ReadLine());
+
+        int horas = minutos / 60;//se utiliza en este contexto para calcular el residuo de la división de los minutos entre 60
+        int minutosRestantes = minutos % 60;//se utiliza para calcular cuántos minutos quedan después de que se han calculado todas las horas completas.
+
+        Console.WriteLine($"{minutos} minutos son {horas} horas y {minutosRestantes} minutos");
